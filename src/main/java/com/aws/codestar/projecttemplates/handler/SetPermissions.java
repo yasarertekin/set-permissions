790dc60e6a02f97f0eeb54e27e718185986b8a67
yasarertekin <yasarertekin@outlook.com>
2017-10-15 20:26:29 +0200

    serverless-perm-01: Volgens serverles architecture on aws boek aanpassing provider

--- src/main/java/com/aws/codestar/projecttemplates/handler/SetPermissions.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/SetPermissions.java
@@ -1,29 +1,24 @@
 package com.aws.codestar.projecttemplates.handler;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
-import com.aws.codestar.projecttemplates.GatewayResponse;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class SetPermissions implements RequestHandler<SNSEvent, Object> {
 
-    private static final AmazonS3 s3 = new AmazonS3Client(new DefaultAWSCredentialsProviderChain());
+    private static final AmazonS3 s3 = new AmazonS3Client(new ProfileCredentialsProvider());
     public String handleRequest(final SNSEvent event, final Context context) {
         LambdaLogger logger = context.getLogger();
 
